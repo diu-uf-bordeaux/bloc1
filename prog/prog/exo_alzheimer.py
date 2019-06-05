d5c957edf8e2129e5b4898158b677541e9a506a1
David Renault <renault@enseirb.fr>
2019-06-05 11:24:13 +0200

    Prog - modify exo_alzheimer.py

--- prog/prog/exo_alzheimer.py
+++ prog/prog/exo_alzheimer.py
@@ -1,24 +1,22 @@
-
-
 import csv
 import random
 
-
-# 0 AD/CN - 1 Genre - 2 Age - 3 Score cognitif - 4 Volume HIPPocampe cm3 - 5 Volume relatif HIPPocampe %
-CLASS = 0
-GENDER = 1
-AGE = 2
-SCORE = 3
-HIPPOVOL = 4
-HIPPOPERCENT = 5
+# Constantes utilisées dans ce module
+CLASS = 0         # AD == malade / CN == contrôle
+GENDER = 1        # Genre
+AGE = 2           # Age
+SCORE = 3         # Score cognitif
+HIPPOVOL = 4      # Volume HIPPocampe (en cm3)
+HIPPOPERCENT = 5  # Volume relatif HIPPocampe (en %, entre 0 et 1)
 
 
 def importAlzheimerData(csvFileName):
     """
-        importe un fichier csv contenant :
+        Importe un fichier csv contenant :
         - une ligne d'intitulés de colonnes
-        - des lignes de format CLASS (string); GENDER(string); AGE(int); SCORE(float); HIPPOVOL(float); HIPPOPERCENT(float)
-        voir https://docs.python.org/fr/3/library/csv.html
+        - des lignes de format CLASS (string); GENDER(string); AGE(int);
+                      SCORE(float); HIPPOVOL(float); HIPPOPERCENT(float)
+        (voir https://docs.python.org/fr/3/library/csv.html)
     """
     with open(csvFileName) as csvfile:
         reader = csv.reader(csvfile, delimiter=';')
@@ -34,117 +32,161 @@ def importAlzheimerData(csvFileName):
 
     return data
 
+
 def sort(data, feature):
-    """ trie une liste de liste vue comme un tableau 2D, 1er indice = ligne, 2eme indice = colonne, suivant la colonne d'indice feature """
+    """ Trie une liste d'enregistrements vue comme un tableau 2D,
+        (1er indice = ligne, 2eme indice = colonne)
+        suivant la colonne d'indice `feature`. """
     data.sort(key= lambda l: l[feature])
 
+
 def extractFeature(data, feature):
-    """ extrait la colonne d'indice feature d'une liste de liste vue comme un tableau 2D, 1er indice = ligne, 2eme indice = colonne """
-    #return[data[i][feature] for i in range(len(data))]
+    """ Extrait la colonne d'indice `feature` d'une liste
+        d'enregistrements vue comme un tableau 2D
+        (1er indice = ligne, 2eme indice = colonne) """
+    # return[data[i][feature] for i in range(len(data))]
     return[l[feature] for l in data]
-    
+
+
 def meanFeature(data, feature, rawBeginIndex, rawEndIndex):
-    """ calcule la moyenne d'une colonne entre les lignes d'indices rawBeginIndex, rawEndIndex """
-    featureData = extractFeature(data, feature)      
+    """ Calcule la moyenne d'une colonne entre les lignes
+        d'indices rawBeginIndex et rawEndIndex de la liste `data`."""
+    assert(rawEndIndex != rawBeginIndex)
+    featureData = extractFeature(data, feature)
     sum = featureData[rawBeginIndex]
     for i in range(rawBeginIndex + 1, rawEndIndex + 1):
-        sum += featureData[i]    
+        sum += featureData[i]
     return sum / (rawEndIndex - rawBeginIndex + 1)
 
+
 def separateAdCn(data):
-    """ teste si dans la colonne d'indice CLASS toutes les valeurs "AD" sont au début """
+    """ Teste si dans la colonne d'indice CLASS,
+        toutes les valeurs "AD" sont au début. """
     n = len(data)
     for i in range(n-1):
         if data[i][CLASS] == "CN" and data[i+1][CLASS] == "AD":
             return False
     return True
 
+
+def containBothClasses(data):
+    """ Teste si la liste `data` contient bien des données
+        appartenant aux deux classes AD et CN. """
+    ads = [ d for d in data if d[CLASS] == "AD" ]
+    cns = [ d for d in data if d[CLASS] == "CN" ]
+    return (len(ads) > 0) and (len(cns) > 0)
+
+
 def computeMeans(data, feature):
     """
-        calcule la moyenne du groupe AD et la moyenne du groupe CN pour la caractéristique feature
-        suppose que feature est une caractéristique numérique (AGE, SCORE, HIPPOVOL, HIPPOPERCENT)
-        suppose que les deux classes apparaissent dans les données
+        Calcule la moyenne du groupe AD et du groupe CN
+        pour la caractéristique `feature`.
+        Suppose que feature est une caractéristique numérique
+                            (AGE, SCORE, HIPPOVOL, HIPPOPERCENT)
+        Suppose que les deux classes apparaissent dans les données.
     """
-    # trie la table de façon à avoir le groupe AD au début (AD avant CN)
+    # Trie la table de façon à avoir le groupe AD au début (AD avant CN)
     sort(data, CLASS)
-    # calcule l'indice du premier élément de la classe CN
+    # Calcule l'indice du premier élément de la classe CN
     i = 0
     for l in data:
         if l[CLASS] == "CN":
             break
         i += 1
-    return meanFeature(data, feature, 0, i), meanFeature(data, feature, i+1, len(data) - 1)
+    return meanFeature(data, feature, 0, i), \
+        meanFeature(data, feature, i+1, len(data) - 1)
 
 
 def classificationErrorRate(data, feature, meanAD, meanCN):
     """
-       classifie chaque individu suivant la caractéristique feature et les moyennes fournies
-       la classe est celle qui correspond à la moyenne la plus proche
-       si cette classe est différente de celle de l'individu, il y a erreur de classification
+       Classifie chaque individu dans `data` suivant la caractéristique
+       `feature` et les moyennes fournies.
+       La classe est celle qui correspond à la moyenne la plus proche.
+       Il y a erreur de classification si cette classe est différente
+       de celle de l'individu.
     """
     nbErrors = 0
-    for l in data :
-        if abs(l[feature] - meanAD) < abs(l[feature] - meanCN) : # on classifie comme AD
+    for l in data:
+        if abs(l[feature] - meanAD) < abs(l[feature] - meanCN): # on classifie comme AD
             if l[CLASS] != "AD":
                 nbErrors += 1
     return round(nbErrors*100/len(data))
 
+
 def subList(l, listInd):
     """
-       crée une sous-liste d'une liste à partir de la liste des indices des éléments à garder
+       Crée une sous-liste d'une liste `l` à partir de la liste des
+       indices des éléments à conserver.
     """
     subL = []
     for i in listInd:
         subL += [l[i]]
     return subL
 
+
 def splitList(l, nb):
     """
-       crée aléatoirement deux sous listes sans intersection à partir de la liste l
-       la première contient nb éléments, la seconde len(l) - nb éléments
+       Crée aléatoirement deux sous listes sans intersection à partir
+       d'une liste `l`. La première contient nb éléments, la seconde
+       len(l) - nb éléments.
     """
     listInd = list(range(len(data)))
-    random.shuffle(listInd) # une permutation aléatoire des indices de ligne
+    random.shuffle(listInd) # Une permutation aléatoire des indices de ligne
     lInd1 = listInd[0:nb]
     lInd2 = listInd[nb: len(data)]
-    return subList(data, lInd1), subList(data, lInd2)                                 
-                                 
+    return subList(data, lInd1), subList(data, lInd2)
+
+
+################################################################
 if __name__ == '__main__':
-    
-    data = importAlzheimerData("Alzheimer.csv")
+
+    data = importAlzheimerData("Alzheimer.2.csv")
     n = len(data)
-    
-    # extraction d'une colonne
+
+    # Extraction d'une colonne
     scores = extractFeature(data, SCORE)
     print(scores)
-    
-    # calcul de la moyenne d'une colonne
+
+    # Calcul de la moyenne d'une colonne
     m = 0
     for val in scores:
         m += val
     m /= len(scores)
-    
-    print("moyenne des scores cognitifs : ", meanFeature(data, SCORE, 0, n-1), m)
-    
-    # trier suivant une colonne
+
+    print("Moyenne des scores cognitifs : ", meanFeature(data, SCORE, 0, n-1), m)
+
+    # Trie les données selon leur SCORE
     sort(data, SCORE)
     print(extractFeature(data, SCORE))
     print(extractFeature(data, CLASS))
-    print(separateAdCn(data)) # trier suivant les scores ne sépare pas les deux classes mais presque!
-    
-    # classification
+    # Trier suivant les scores ne sépare pas les deux classes mais presque!
+    print("Le score sépare les classes ? : %s" % (separateAdCn(data),))
+
+    # Classification
     trainingData, testingData = splitList(data, n*2//3)
-    
+
+    # Vérification
+    if (not containBothClasses(trainingData)) or \
+       (not containBothClasses(testingData)):
+        raise "Découpage des données incorrect"
+
+    # Affichage des résultats
     meanAD, meanCN = computeMeans(trainingData, SCORE)
-    print("erreur score : ", classificationErrorRate(testingData, SCORE, meanAD, meanCN), "%")
-    
+    print("Erreur score : ",
+          classificationErrorRate(testingData, SCORE,
+                                  meanAD, meanCN), "%")
+
     meanAD, meanCN = computeMeans(trainingData, HIPPOVOL)
-    print("erreur volume hippocampe : ", classificationErrorRate(testingData, HIPPOVOL, meanAD, meanCN), "%")
-    
+    print("Erreur volume hippocampe : ",
+          classificationErrorRate(testingData, HIPPOVOL,
+                                  meanAD, meanCN), "%")
+
     meanAD, meanCN = computeMeans(trainingData, HIPPOPERCENT)
-    print("erreur volume relatif hippocampe: ", classificationErrorRate(testingData, HIPPOPERCENT, meanAD, meanCN), "%")
-    
+    print("Erreur volume relatif hippocampe: ",
+          classificationErrorRate(testingData, HIPPOPERCENT,
+                                  meanAD, meanCN), "%")
+
     meanAD, meanCN = computeMeans(trainingData, AGE)
-    print("erreur age: ", classificationErrorRate(testingData, AGE, meanAD, meanCN), "%")
-    
-        
+    print("Erreur age: ",
+          classificationErrorRate(testingData, AGE,
+                                  meanAD, meanCN), "%")
